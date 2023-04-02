f5f44fa68dcebd0137122191d617e0ed5aac0024
JustK1134 <khoilucforeducation@gmail.com>
2023-04-02 18:06:36 +0700

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1x^9%mth&(d)vkc+s9qmak5j-eyzuth2^-=o(3y)mcrlz!3h49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["muddled-car-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
